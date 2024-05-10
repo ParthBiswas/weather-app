e1b1371932d6882535378765ccc7971e80a0c5cb
Parth Biswas <Parthbiswas@outlook.com>
2024-05-10 23:24:11 +0530

    u

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+//this is writen for just push
 var app = express();
 
 // view engine setup
